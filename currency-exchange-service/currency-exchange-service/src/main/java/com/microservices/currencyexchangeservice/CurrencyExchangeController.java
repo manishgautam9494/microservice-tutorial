4b1206bc391bc534662c0b6a20bf6e8a94c83dde
manish <manishgautam9494@gmail.com>
2022-12-17 22:20:05 +0530

    changes in API gateway service and its microservices

--- currency-exchange-service/currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -4,9 +4,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController 
+@RequestMapping("exchange")
 public class CurrencyExchangeController 
 {
 @Autowired
